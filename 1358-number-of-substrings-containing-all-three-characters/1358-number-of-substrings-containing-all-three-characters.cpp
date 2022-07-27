514e5280d91824422125724d9a727fb3b370fa6f
Mansi6725 <68185615+Mansi6725@users.noreply.github.com>
2022-07-27 22:29:58 +0530

    Time: 134 ms (17.41%), Space: 18.6 MB (6.17%) - LeetHub

--- 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
+++ 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
@@ -7,12 +7,22 @@ public:
         
         while(j<s.size()){
             mp[s[j]]++;
-            while(mp['a']&&mp['b']&&mp['c']){
-                cnt=cnt+1+s.size()-1-j;
-                mp[s[i]]--;
-                i++;
+//             while(mp['a']&&mp['b']&&mp['c']){
+//                 cnt=cnt+1+s.size()-1-j;
+//                 mp[s[i]]--;
+//                 i++;
                
                 
+//             }
+            while(mp.size()==3){
+                cnt+=s.size()-j;
+                mp[s[i]]--;
+                if(mp[s[i]]==0)
+                {
+                    mp.erase(s[i]);
+                    
+                }
+                i++;
             }
              j++;
         }
