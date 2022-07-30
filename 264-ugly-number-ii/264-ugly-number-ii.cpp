e7730987f8b49df3587dde6ccc2bd2c8be8822aa
Mansi6725 <68185615+Mansi6725@users.noreply.github.com>
2022-07-30 22:57:33 +0530

    Time: 315 ms (5.65%), Space: 47.4 MB (5.01%) - LeetHub

--- 264-ugly-number-ii/264-ugly-number-ii.cpp
+++ 264-ugly-number-ii/264-ugly-number-ii.cpp
@@ -1,56 +1,19 @@
 class Solution {
 public:
     int nthUglyNumber(int n) {
-//         int cnt=0;
-//          int i=1;
-//         while(cnt!=n){
-//           int j=i;
-//           while(1){
-//             if(j==1){
-//                  cnt++;
-//                 if(cnt==n)
-//                     return i;
-//                 i++;
-//                 break;
-//             }
-               
-//             else if(j%2==0)
-//                 j=j/2;
-//             else if(j%3==0)
-//                 j=j/3;
-//             else if (j%5==0)
-//                 j=j/5;
-//             else{
-//                 i++;
-//                  break;
-//             }    
-//         }
-//       }
-//    return -1;
     
-        vector<int> v(n,0);
-        v[0]=1;
-        int indexA=0;
-        int indexB=0;
-        int indexC=0;
-        int mini;
-        int nextA,nextB,nextC;
-        for(int i=1;i<n;i++){
-             nextA=v[indexA]*2;
-             nextB=v[indexB]*3;
-             nextC=v[indexC]*5;
-             mini=min(nextA,min(nextB,nextC));
-             if(mini==nextA)
-                 indexA++;
-            if(mini==nextB)
-                 indexB++;
-            if(mini==nextC)
-                 indexC++;
-            v[i]=mini;
-             
-            
-        }
-        return v[n-1];
+     set<long long> s,p;
+     s.insert(1);
+     while(p.size()!=n){
+         long long num=*s.begin();
+         p.insert(num);
+         s.erase(num);
+         s.insert(2*num);
+         s.insert(3*num);
+         s.insert(5*num);
+     }
+        return *p.rbegin();
+     
     }
 };
 
