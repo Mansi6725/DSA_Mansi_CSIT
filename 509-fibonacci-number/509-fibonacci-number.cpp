5376abf7ab4315a87305289899408144a4896862
Mansi6725 <68185615+Mansi6725@users.noreply.github.com>
2022-08-02 17:35:59 +0530

    Time: 3 ms (58.90%), Space: 5.9 MB (39.89%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -3,7 +3,18 @@ public:
     int fib(int n) {
         if(n==0||n==1)
             return n;
-        else return fib(n-1)+fib(n-2);
+        else{
+            int a=0;
+            int b=1;
+            int c;
+            for(int i=2;i<=n;i++){
+                c=a+b;
+                a=b;
+                b=c;
+            }
+             return c;
+        }
+       
         
     }
 };
