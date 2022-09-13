c2cfeea97a3653ab86d3d70338f1689f112ca428
Mansi6725 <68185615+Mansi6725@users.noreply.github.com>
2022-09-13 10:33:06 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (16.02%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -1,23 +1,18 @@
 class Solution {
+private:
+    int solve(vector<int> &dp,int n){
+         if(n==0||n==1)
+         return 1;
+         if(dp[n]!=-1)
+             return dp[n];
+         dp[n]=solve(dp,n-1)+solve(dp,n-2);
+         return dp[n];
+         
+    }
 public:
     int climbStairs(int n) {
-       if(n<=2)
-            return n;
-        
-        int dp[]={0,1,2};    
-        for(int i=3;i<=n;i++)
-        {
-            //loGIC
-            //DP[3%3]=DP[0] =DP[4%3]+DP[5%3]
-            //DP[4%3]=DP[1] =DP[5%3]+DP[6%3];
-            //hERe performing variables swap
-            //with efficient use of static array and %operator
-            
-            dp[(i)%3]=dp[(i+1)%3]+dp[(i+2)%3];
-        
-        }
-        
-        return dp[n%3];
-        
+        vector<int> dp(n+1,-1);
+        return solve(dp,n);
+     
     }
 };
