531ab135c43f5bb59236d77a17877ea922063fda
Mansi6725 <68185615+Mansi6725@users.noreply.github.com>
2022-09-23 00:21:18 +0530

    Time: 7 ms (9.17%), Space: 6.2 MB (7.07%) - LeetHub

--- 856-score-of-parentheses/856-score-of-parentheses.cpp
+++ 856-score-of-parentheses/856-score-of-parentheses.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int scoreOfParentheses(string s) {
         stack<int> st;
-        int ans=0;
+       
         for(int i=0;i<s.length();i++){
             if(s[i]=='(') 
                 st.push(-1);
@@ -23,6 +23,7 @@ public:
                 }
             }
         }
+         int ans=0;
         while(!st.empty()){
            
             ans+=st.top();
